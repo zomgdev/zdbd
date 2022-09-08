bef2052581cb834f5637a21bda8a31d3c84223d3
Mikhail Butalin <m.butalin@gmail.com>
2022-09-08 21:53:53 +0300

    Add project files.

---
+++ server/server.cpp
@@ -0,0 +1,16 @@
+﻿// server.cpp : Defines the entry point for the application.
+//
+
+#include "server.h"
+#include "test1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+
+	test1();
+
+	return 0;
+}
---
+++ server/test1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+
+void test1() {
+	cout << "Function test1 called..." << endl;
+}
